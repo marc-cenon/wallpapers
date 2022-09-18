65b3f2eec91ee139cfcfb08ba176ff9fe8276be7
CENON <cenon@PC-macbook.local>
2022-09-18 21:32:25 +0200

    update script

--- rename.py
+++ rename.py
@@ -3,10 +3,17 @@ import os
 path = "."
 files = os.listdir(path)
 index=1
-        
-for index, file in enumerate(files):
-    if file == "rename.py":
-        continue
-    else:
-        os.rename(file, 'wall_' + str(index)+ '.jpg')
-        index = index+1
+
+def rename_file(files):
+    for index, file in enumerate(files):
+        if file == "rename.py" or ".git":
+            continue
+        else:
+            os.rename(file, 'wall_' + str(index)+ '.jpg')
+            index = index+1
+
+def main():
+    rename_file(files)
+
+if __name__ == '__main__':
+    main()
