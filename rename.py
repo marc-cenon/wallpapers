21429f88c95cb70e9e1046f6213d5c1f84f4adc5
marc-cenon <marc.cenon33@gmail.com>
2022-09-20 10:45:31 +0200

    update

--- rename.py
+++ rename.py
@@ -6,14 +6,12 @@ index=1
 
 def rename_file(files):
     for index, file in enumerate(files):
+        print(file)
         if file == "rename.py" or ".git":
             continue
         else:
             os.rename(file, 'wall_' + str(index)+ '.jpg')
             index = index+1
 
-def main():
-    rename_file(files)
-
 if __name__ == '__main__':
-    main()
+    rename_file(files)
